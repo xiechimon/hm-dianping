b88510d37483df82d1af78f1046ff8ab397f225e
Xiechimon <xiechimon@qq.com>
2025-12-21 00:19:04 +0800

    feat: add SMS verification code login and registration

--- src/main/java/com/xmon/service/IUserService.java
+++ src/main/java/com/xmon/service/IUserService.java
@@ -1,6 +1,7 @@
 package com.xmon.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.xmon.dto.LoginFormDTO;
 import com.xmon.dto.Result;
 import com.xmon.entity.User;
 import jakarta.servlet.http.HttpSession;
@@ -16,4 +17,6 @@ import jakarta.servlet.http.HttpSession;
 public interface IUserService extends IService<User> {
 
     Result sendCode(String phone, HttpSession session);
+
+    Result login(LoginFormDTO loginForm, HttpSession session);
 }
--- src/main/java/com/xmon/service/impl/UserServiceImpl.java
+++ src/main/java/com/xmon/service/impl/UserServiceImpl.java
@@ -2,11 +2,13 @@ package com.xmon.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.xmon.dto.LoginFormDTO;
 import com.xmon.dto.Result;
 import com.xmon.entity.User;
 import com.xmon.mapper.UserMapper;
 import com.xmon.service.IUserService;
 import com.xmon.utils.RegexUtils;
+import com.xmon.utils.SystemConstants;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -42,5 +44,46 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
         // 6.返回OK
         return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 1.校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误！");
+        }
+
+        // 2.校验验证码，是否为空并且是否一致
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        if (cacheCode == null || !cacheCode.toString().equals(code)) {
+            // 3.不一致，报错
+            return Result.fail("验证码为空或不一致，请重新输入！");
+        }
+
+        // 4.一致，根据手机号查询用户
+        User user = query().eq("phone", phone).one();
+
+        // 5.判断用户是否存在
+        if (user == null) {
+            // 6.不存在，创建新用户并保存到数据库
+            user = createUserWithPhone(phone);
+        }
+
+        // 7.（不存在和存在都要）保存用户信息到session
+        session.setAttribute("user", user);
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        // 1.创建用户
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(SystemConstants.USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+
+        // 2.保存用户
+        save(user);
+        return user;
     }
 }
