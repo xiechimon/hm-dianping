8c2961cd852078cd3d771e2c823bc3778010ef0f
Xiechimon <xiechimon@qq.com>
2026-03-04 22:50:18 +0800

    feat: add redis cache for shop

--- src/main/java/com/xmon/service/IShopService.java
+++ src/main/java/com/xmon/service/IShopService.java
@@ -1,5 +1,6 @@
 package com.xmon.service;
 
+import com.xmon.dto.Result;
 import com.xmon.entity.Shop;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IShopService extends IService<Shop> {
 
+    Result queryById(Long id);
 }
--- src/main/java/com/xmon/service/impl/ShopServiceImpl.java
+++ src/main/java/com/xmon/service/impl/ShopServiceImpl.java
@@ -1,14 +1,21 @@
 package com.xmon.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.xmon.dto.Result;
 import com.xmon.entity.Shop;
 import com.xmon.mapper.ShopMapper;
 import com.xmon.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.xmon.utils.RedisConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -17,4 +24,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryById(Long id) {
+        // 1. 从 Redis 查询商户缓存
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        // 2. 判断是否存在
+        if (StrUtil.isNotBlank(shopJson)) {
+            // 3. 存在，直接返回
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+        // 4. 不存在，根据 id 查询数据库
+        Shop shop = getById(id);
+        // 4.1 不存在，返回错误
+        if (shop == null) {
+            return Result.fail("店铺不存在！");
+        }
+        // 4.2 存在，写入 Redis
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+
+        // 5. 返回
+        return Result.ok(shop);
+    }
 }
