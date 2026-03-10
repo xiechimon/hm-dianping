f18c9fa84ee73ab6ac7d374f4c0683b68c7c47e7
Xiechimon <xiechimon@qq.com>
2026-03-10 14:51:22 +0800

    feat: global unique ID by Redis

--- src/test/java/com/xmon/HmDianpingApplicationTests.java
+++ src/test/java/com/xmon/HmDianpingApplicationTests.java
@@ -4,10 +4,14 @@ import com.xmon.entity.Shop;
 import com.xmon.service.impl.ShopServiceImpl;
 import com.xmon.utils.CacheClient;
 import com.xmon.utils.RedisConstants;
+import com.xmon.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
@@ -18,6 +22,31 @@ class HmDianpingApplicationTests {
 
     @Resource
     private ShopServiceImpl shopService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(500);
+
+    @Test
+    void testIdWork() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(300);
+        Runnable runnable = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            countDownLatch.countDown();
+        };
+
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            executorService.submit(runnable);
+        }
+        countDownLatch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time: " + (end - begin));
+    }
 
     @Test
     void saveShop() throws InterruptedException {
